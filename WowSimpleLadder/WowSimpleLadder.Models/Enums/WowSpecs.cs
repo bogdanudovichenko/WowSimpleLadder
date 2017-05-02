8c02afac609694ddc502167a5b154d4d7e08f041
bogdanudovichenkodev <bogdanudovichenkodev@gmail.com>
2017-05-02 20:53:32 +0300

    finish working with specs enum

--- WowSimpleLadder/WowSimpleLadder.Models/Enums/WowSpecs.cs
+++ WowSimpleLadder/WowSimpleLadder.Models/Enums/WowSpecs.cs
@@ -4,14 +4,57 @@
     {
         //paladin
         HolyPaladin = 65,
+        ProtectionPaladin = 66,
+        RetributionPaladin = 70,
 
         //warrior
         ArmsWarrior = 71,
+        FuryWarrior = 72,
+        ProtectionWarrior = 73,
+
+        //mage
+        ArcaneMage = 62,
+        FireMage = 63,
+        FrostMage = 64,
+
         //druid
+        BalanceDruid = 102,
+        FeralDruid = 103,
+        GuardianDruid = 104,
         RestorationDruid = 105,
+
         //dk
+        BloodDk = 250,
+        FrostDk = 251,
         UnholyDk = 252,
+
+        //hunter
+        BeastMastery = 253,
+        Marksmanship = 254,
+        Survival = 255,
+
+        //priest
+        Discipline = 256,
+        Holy = 257,
+        Shadow = 258,
+
+        //rogue
+        Assassination = 259,
+        Outlaw = 260,
+        Subtlety = 261,
+
         //shaman
-        RestorationShaman = 264
+        ElementalShaman = 262,
+        EnchancementShaman = 263,
+        RestorationShaman = 264,
+
+        //warlock
+        AfflictionWarlock = 265,
+        DemonologyWarlock = 264,
+        Destruction = 267,
+
+        //demon hunter
+        HavokDh = 577,
+        VengeanceDh = 581
     }
 }
