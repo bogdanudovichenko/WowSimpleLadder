578534d4e581bc03b56802739820169d6e8e2a1e
bogdanudovichenkodev <bogdanudovichenkodev@gmail.com>
2017-06-03 09:28:04 +0300

    add dropdown markup

--- WowSimpleLadder/WowSimpleLadder.Web/Scripts/src/Modules/Controls/DropDownControl.js
+++ WowSimpleLadder/WowSimpleLadder.Web/Scripts/src/Modules/Controls/DropDownControl.js
@@ -35,45 +35,80 @@
     }
 
     DropDownControl.prototype._render = function () {
-        var dropDownWrapper = document.createElement('div');
-        dropDownWrapper.classList.add('drop-down-control-wrapper');
-        dropDownWrapper.setAttribute('tabindex', 1);
-
-        dropDownWrapper.addEventListener('click', function () {
-            if (!this.classList.contains('active')) {
-                this.classList.add('active');
-            } else {
-                this.classList.remove('active');
-            }
-        });
-
-        var ul = document.createElement('ul');
-        ul.classList.add('dropdown-control-list');
-
-        var data = this._data;
-        var dataLength = data.length;
-
-        for (var i = 0; i < dataLength; i++) {
-            var item = data[i];
-
-            if (!item) {
-                continue;
-            }
-
-            var li = this._createLi(item);
-            ul.appendChild(li);
+        window.myFunction = function() {
+            document.getElementById("myDropdown").classList.toggle("show");
         }
 
-        dropDownWrapper.appendChild(ul);
+        window.onclick = function (event) {
+            if (!event.target.matches('.dropbtn')) {
+
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+
+        var markup = `<div class="dropdown">
+                        <div onclick="myFunction()" class="dropbtn">Select class</div>
+                        <div id="myDropdown" class="dropdown-content">
+                            <span>Link 1</span>
+                            <span>Link 2</span>
+                            <span>Link 3</span>
+                        </div>
+                      </div>`;
 
         var targetToRender = document.querySelector(this._selector);
-
         if (!targetToRender) {
             console.error('dropdown cant find target to render on selector: ' + this._selector);
             return;
         }
 
-        targetToRender.appendChild(dropDownWrapper);
+        targetToRender.innerHTML = markup;
+
+        // var dropDownWrapper = document.createElement('div');
+        // dropDownWrapper.classList.add('drop-down-control-wrapper');
+        // dropDownWrapper.setAttribute('tabindex', 1);
+
+        // dropDownWrapper.addEventListener('click', function () {
+        //     if (!this.classList.contains('active')) {
+        //         this.classList.add('active');
+        //     } else {
+        //         this.classList.remove('active');
+        //     }
+        // });
+
+        // var ul = document.createElement('ul');
+        // ul.classList.add('dropdown-control-list');
+
+        // var data = this._data;
+        // var dataLength = data.length;
+
+        // for (var i = 0; i < dataLength; i++) {
+        //     var item = data[i];
+
+        //     if (!item) {
+        //         continue;
+        //     }
+
+        //     var li = this._createLi(item);
+        //     ul.appendChild(li);
+        // }
+
+        // dropDownWrapper.appendChild(ul);
+
+        // var targetToRender = document.querySelector(this._selector);
+
+        // if (!targetToRender) {
+        //     console.error('dropdown cant find target to render on selector: ' + this._selector);
+        //     return;
+        // }
+
+        // targetToRender.appendChild(dropDownWrapper);
     }
 
     DropDownControl.prototype._createLi = function (item) {
